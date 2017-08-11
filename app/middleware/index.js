60cdddfbbbefaec99329a7eaa1ff893ce34e548a
zuoshaohui <zuoshaohui@agree.com.cn>
2017-08-11 16:09:36 +0800

    update

--- app/middleware/index.js
+++ app/middleware/index.js
@@ -1,12 +1,12 @@
 'use strict';
 
 import compose from 'koa-compose';
-import checkauth from './checkauth';
+// import checkauth from './checkauth';
 
 export default function middleware() {
     return compose(
         [
-            checkauth()
+            // checkauth()
         ]
     )
 }
